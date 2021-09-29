1dda93aca34720650387ea14af30314554f6e4bd
haind@acaziasoft.com <haind@acaziasoft.com>
2021-09-30 00:30:11 +0700

    add data

--- src/app.js
+++ src/app.js
@@ -1,14 +1,51 @@
 const express = require('express')
+const fs = require('fs')
 
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/',(req,res)=>{
-    res.send({
-        data: 'hello 123'
+const addNote1 = function(file_name){
+    const notes = loadNotes(file_name);
+    notes.push({
+        title: "file_name" + Date.now(),
+        date: Date.now()
     })
+    saveNotes(notes,file_name)
+}
+const saveNotes = function(notes,file_name){
+    const dataJSON = JSON.stringify(notes)
+    fs.writeFileSync(file_name, dataJSON)
+}
+const loadNotes = function(file_name){
+    try {
+        const dataBuffer = fs.readFileSync(file_name)
+        const dataJSON = dataBuffer.toString()
+        return JSON.parse(dataJSON)
+    } catch (error) {
+        return []
+    }
+        
+}
+const data = {
+    title: 'Approve',
+    date: Date.now()
+}
+const data2 = {
+    title: 'Reject',
+    date: Date.now()
+}
+const dataJSON = JSON.stringify(data)
+const dataJSON2 = JSON.stringify(data2)
+
+app.get('/approve',(req,res)=>{
+    addNote1('approve.json')
+    res.send({data:'approve'})
 })
 
+app.get('/reject',(req,res)=>{
+    addNote1('reject.json')
+    res.send({data:'reject'})
+})
 app.listen(port,()=>{
     console.log('port: ' + port);
 })
